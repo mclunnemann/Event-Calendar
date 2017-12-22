ba338ad4ffecf7e6210c661b6f0fa41c8278402b
mclunnemann <mclunnemann@gmail.com>
2017-12-22 14:48:01 -0500

    Add files via upload

---
+++ DesktopCalendar/DesktopCalendar/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace DesktopCalendar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.rtbInfo = new System.Windows.Forms.RichTextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.lblDisplayInfo = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(12, 7);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // rtbInfo
+            // 
+            this.rtbInfo.Location = new System.Drawing.Point(263, 7);
+            this.rtbInfo.Name = "rtbInfo";
+            this.rtbInfo.Size = new System.Drawing.Size(419, 162);
+            this.rtbInfo.TabIndex = 1;
+            this.rtbInfo.Text = "";
+            this.rtbInfo.TextChanged += new System.EventHandler(this.rtbInfo_TextChanged);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(526, 175);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(607, 175);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 3;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // lblDisplayInfo
+            // 
+            this.lblDisplayInfo.AutoSize = true;
+            this.lblDisplayInfo.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisplayInfo.Location = new System.Drawing.Point(11, 196);
+            this.lblDisplayInfo.MaximumSize = new System.Drawing.Size(400, 0);
+            this.lblDisplayInfo.Name = "lblDisplayInfo";
+            this.lblDisplayInfo.Size = new System.Drawing.Size(40, 23);
+            this.lblDisplayInfo.TabIndex = 4;
+            this.lblDisplayInfo.Text = "Text";
+            this.lblDisplayInfo.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 178);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(283, 18);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "The selected day\'s events will show up here:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(694, 231);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblDisplayInfo);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.rtbInfo);
+            this.Controls.Add(this.monthCalendar1);
+            this.Name = "Form1";
+            this.Text = "Events Calendar";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.RichTextBox rtbInfo;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Label lblDisplayInfo;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DesktopCalendar/DesktopCalendar/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace DesktopCalendar
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<DateTime, string> CalendarInfo = new Dictionary<DateTime, string>();
+        DateTime date = new DateTime();
+        string dateInfo = "";
+        DateTime StartDate = new DateTime();
+        DateTime EndingDate = new DateTime();
+        DateTime[] dt;        
+        string file = "TEST_DateAppointments.txt";
+        string tempFile = "DateAppointments_TEMP.txt";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            date = e.Start;
+            foreach (KeyValuePair<DateTime, string> entry in CalendarInfo)
+            {
+                if (e.Start == entry.Key)
+                {
+                    rtbInfo.Text = entry.Value;
+                    lblDisplayInfo.Visible = true;
+                    lblDisplayInfo.Text = entry.Value;
+                    ResizeForm();
+                    btnRemove.Enabled = true;
+                    break;
+                }
+                else
+                {
+                    rtbInfo.Text = "";
+                    lblDisplayInfo.Visible = false;
+                    btnRemove.Enabled = false;
+                    ResizeForm();
+                }
+            }
+
+            string strDate = Convert.ToString(date.Month + "/" + date.Day + "/" + date.Year);
+            label1.Text = Convert.ToString("Events on " + strDate + ":");
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string strDate = Convert.ToString(date.Month + "/" + date.Day + "/" + date.Year);
+            dateInfo = rtbInfo.Text;
+            //MessageBox.Show(strDate + " - " + dateInfo);
+            CalendarInfo[Convert.ToDateTime(strDate)] = dateInfo;
+            lblDisplayInfo.Text = dateInfo;
+            WriteToFile();
+            formLoad();
+            ResizeForm();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            formLoad();
+            date = monthCalendar1.TodayDate;
+        }
+
+        private void rtbInfo_TextChanged(object sender, EventArgs e)
+        {
+            if (rtbInfo.Text != "")
+            {
+                btnSubmit.Enabled = true;              
+            }
+            else
+            {
+                btnSubmit.Enabled = false;
+            }
+        }
+
+        private void WriteToFile()
+        {
+            using (StreamWriter writer = new StreamWriter(file)) {
+                foreach (KeyValuePair<DateTime, string> entry in CalendarInfo)
+                {
+                    string replacement = Regex.Replace(entry.Value, @"\t|\n|\r", "!NEWLINE!");
+                    writer.WriteLine(entry.Key + "|" + replacement);
+                }
+                writer.Close();
+            }
+        }
+
+        private void ReadFile()
+        {
+            string line;
+            DateTime cutOffDate = (monthCalendar1.TodayDate.AddDays(-30));
+            if (!File.Exists(file))
+            {
+                File.Create(file);
+            }
+            else
+            {
+                if (new FileInfo(file).Length != 0)
+                {
+                    using (StreamReader reader = new StreamReader(file))
+                    {
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            string[] arr = line.Split('|');
+                            if (Convert.ToDateTime(arr[0]) >= cutOffDate)
+                            {
+                                string replacement = Regex.Replace(arr[1],"!NEWLINE!","\n");
+                                CalendarInfo[Convert.ToDateTime(arr[0])] = replacement;                                
+                            }
+                        }
+                        reader.Close();
+                    }
+                }
+            }            
+        }
+
+        public void DeleteFromFile()
+        {            
+            if (new FileInfo(file).Length != 0)
+            {
+                using (StreamReader reader = new StreamReader(file))
+                using (StreamWriter writer = new StreamWriter(tempFile))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string[] arr = line.Split('|');
+                        if (Convert.ToDateTime(arr[0]) != monthCalendar1.SelectionStart)
+                        {
+                            writer.WriteLine(line);
+                        }
+                    }
+                }
+                File.Delete(file);
+                File.Move(tempFile, file);
+            }
+            formLoad();
+        }
+        
+        public void formLoad()
+        {
+            btnSubmit.Enabled = false;
+            btnRemove.Enabled = false;
+            lblDisplayInfo.Visible = false;
+            ReadFile();
+            StartDate = (monthCalendar1.TodayDate.AddDays(-29));
+            EndingDate = (monthCalendar1.TodayDate.AddDays(60));
+            dt = new DateTime[CalendarInfo.Count];
+            int i = 0;
+            foreach (KeyValuePair<DateTime, string> entry in CalendarInfo)
+            {
+                if (entry.Key >= StartDate && entry.Key <= EndingDate)
+                {
+                    dt[i] = entry.Key;
+                    i++;
+                }
+            }
+            monthCalendar1.BoldedDates = dt;
+            monthCalendar1.UpdateBoldedDates();
+            ;//breakpoint
+            monthCalendar1.SelectionStart = monthCalendar1.TodayDate;
+            date = monthCalendar1.SelectionStart;
+            if (CalendarInfo.ContainsKey(monthCalendar1.TodayDate))
+            {
+                if (CalendarInfo[monthCalendar1.TodayDate] != "")
+                {
+                    rtbInfo.Text = CalendarInfo[monthCalendar1.TodayDate];
+                    btnRemove.Enabled = true;
+                    lblDisplayInfo.Visible = true;
+                    lblDisplayInfo.Text = CalendarInfo[monthCalendar1.TodayDate];
+                }
+            }
+            else
+            {
+                lblDisplayInfo.Text = "";
+            }
+            string strDate = Convert.ToString(date.Month + "/" + date.Day + "/" + date.Year);
+            label1.Text = Convert.ToString("Events on " + strDate + ":");
+            ResizeForm();
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            CalendarInfo.Remove(monthCalendar1.SelectionStart);
+            DeleteFromFile();
+            rtbInfo.Text = "";
+            formLoad();
+        }
+
+        private void ResizeForm()
+        {
+            this.Update();
+            this.Size = new System.Drawing.Size(this.Width, ((monthCalendar1.Height + 10) + (label1.Height + 10) + (lblDisplayInfo.Height + 50)));
+            this.Padding = new Padding(0, 0, 0, 10);
+            this.AutoSize = true;
+            this.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            this.Update();
+        }
+
+    }
+}
